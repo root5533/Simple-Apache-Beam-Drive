a900f1c4b5aa56025313504adc1f8ad2ddad6ed7
Tuan <ymuraad@gmail.com>
2018-09-06 12:20:46 +0530

    Add pipe transform to return length of element
    Writes out length of each element read from input.txt delimeter by \n and create output.txt

--- src/main/java/org/wso2/App.java
+++ src/main/java/org/wso2/App.java
@@ -1,13 +1,55 @@
 package org.wso2;
 
-/**
- * Hello world!
- *
- */
-public class App 
+import org.apache.beam.sdk.Pipeline;
+import org.apache.beam.sdk.io.TextIO;
+import org.apache.beam.sdk.options.PipelineOptions;
+import org.apache.beam.sdk.options.PipelineOptionsFactory;
+import org.apache.beam.sdk.transforms.DoFn;
+import org.apache.beam.sdk.transforms.MapElements;
+import org.apache.beam.sdk.transforms.ParDo;
+import org.apache.beam.sdk.values.PCollection;
+import org.apache.beam.sdk.values.TypeDescriptor;
+import org.apache.beam.sdk.values.TypeDescriptors;
+
+import javax.xml.soap.Text;
+
+public class App
 {
+
+    private static class ComputeWordLength extends DoFn<String, Integer> {
+
+        @ProcessElement
+        public void processElement(@Element String word, OutputReceiver<Integer> out) {
+            System.out.println("Word is >>>>>>>>>>>>>>>>>>>>>>>>>> " + word);
+            out.output(word.length());
+        }
+
+    }
+
+    private static class IntToString extends DoFn<Integer, String> {
+
+        @ProcessElement
+        public void processElement(@Element Integer number, OutputReceiver<String> out) {
+            out.output(Integer.toString(number));
+        }
+
+    }
+
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+
+        PipelineOptions options = PipelineOptionsFactory.create();
+        Pipeline pipe = Pipeline.create(options);
+
+        PCollection<String> lines = pipe.apply("Read input text", TextIO.read().from("input.txt"));
+
+        PCollection<Integer> lengthCount = lines.apply(ParDo.of(new ComputeWordLength()));
+
+        lengthCount.apply(ParDo.of(new IntToString()))
+                .apply("Writing output text", TextIO.write().to("output.txt"));
+
+        pipe.run();
+
     }
 }
